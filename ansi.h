22fc0efc859e3cc022da5465b83879f0067071b3
FlipTheEgg <38611657+FlipTheEgg@users.noreply.github.com>
2018-06-04 10:56:56 +0200

    Added ansi.h
    Also corrected spelling error: clrsrc(); -> clrscr();

---
+++ ansi.h
@@ -0,0 +1,20 @@
+
+
+#ifndef _ANSI_H_
+#define _ANSI_H_
+
+void fgcolor(int foreground);
+void bgcolor(int background);
+void color(int foreground, int background);
+void resetbgcolor();
+void clrscr();
+void clrrol();
+void gotoxy(int x, int y);
+void underline(uint8_t on);
+void blink(uint8_t on);
+void inverse(uint8_t on);
+void reset();
+void window(int ax, int ay, int bx, int by, int style, char[] title);
+
+
+#endif /* _ANSI_H_ */
