b42f11853c2e71bdc6d647e643eb052372a30321
FlipTheEgg <38611657+FlipTheEgg@users.noreply.github.com>
2018-06-04 10:42:26 +0200

    Add files via upload

---
+++ ansi.c
@@ -0,0 +1,189 @@
+#define ESC 0x1B
+
+void fgcolor(int foreground) {
+/*  Value      foreground     Value     foreground
+    ------------------------------------------------
+      0        Black            8       Dark Gray
+      1        Red              9       Light Red
+      2        Green           10       Light Green
+      3        Brown           11       Yellow
+      4        Blue            12       Light Blue
+      5        Purple          13       Light Purple
+      6        Cyan            14       Light Cyan
+      7        Light Gray      15       White
+*/
+  int type = 22;             // normal text
+	if (foreground > 7) {
+	  type = 1;                // bold text
+		foreground -= 8;
+	}
+  printf("%c[%d;%dm", ESC, type, foreground+30);
+}
+
+void bgcolor(int background) {
+/* IMPORTANT:   When you first use this function you cannot get back to true white background in HyperTerminal.
+   Why is that? Because ANSI does not support true white background (ANSI white is gray to most human eyes).
+                The designers of HyperTerminal, however, preferred black text on white background, which is why
+                the colors are initially like that, but when the background color is first changed there is no
+ 	              way comming back.
+   Hint:        Use resetbgcolor(); clrscr(); to force HyperTerminal into gray text on black background.
+
+    Value      Color      
+    ------------------
+      0        Black
+      1        Red
+      2        Green
+      3        Brown
+      4        Blue
+      5        Purple
+      6        Cyan
+      7        Gray
+*/
+  printf("%c[%dm", ESC, background+40);
+}
+
+void color(int foreground, int background) {
+// combination of fgcolor() and bgcolor() - uses less bandwidth
+  int type = 22;             // normal text
+	if (foreground > 7) {
+	  type = 1;                // bold text
+		foreground -= 8;
+	}
+  printf("%c[%d;%d;%dm", ESC, type, foreground+30, background+40);
+}
+
+void resetbgcolor() {
+// gray on black text, no underline, no blink, no reverse
+  printf("%c[m", ESC);  
+}
+
+void clrsrc() {
+    printf("%c[2J",ESC);
+}
+
+void clrrol() {
+    printf("%c[K",ESC);
+}
+
+void gotoxy(int x, int y) {
+    //printf("%c[H",ESC);
+    printf("%c[%d;%df",ESC,y,x);
+}
+
+void underline(uint8_t on) {
+  on ? printf("%c[4m",ESC) : printf("%c[24m",ESC);
+}
+
+void blink(uint8_t on) {
+  on ? printf("%c[5m",ESC) : printf("%c[25m",ESC);
+}
+
+void inverse(uint8_t on) {
+  on ? printf("%c[7m",ESC) : printf("%c[27m",ESC);
+}
+
+//Reset formatting.
+void reset() {
+  printf("%c[m",ESC);
+}
+
+void window(int ax, int ay, int bx, int by, int style, char[] title) {
+
+  //TODO : Should colors be supported?
+
+  // a must be TL, b must be BR.
+  if(ax >= bx) return;
+  if(ay >= by) return;
+
+  //TODO : Test for <= 0 and >= terminal size.
+  // Should out of range box placement be handled here as an input erroe, or rendered?
+
+  uint8_t BG; // Background
+  uint8_t VL; // Vertical Line
+  uint8_t HL; // Horsizontal Line
+  uint8_t TL; // Top Left corner
+  uint8_t TR; // Top Right corner
+  uint8_t BL; // Bottom Left corner
+  uint8_t BR; // Bottom Right corner
+  //Should these be chars?
+
+
+  switch(style) {
+
+    // STYLE 1
+    // - Double line
+    // - Title on border w. decoration
+    case 1 :
+      BG = 32;
+      vertical = 186;
+      horizontal = 205;
+      TL = 201;
+      TR = 187;
+      BL = 200;
+      BR = 188;
+    break;
+
+    // DEFAULT STYLE
+    // - single line
+    // - title within border
+    // Implicit style 0
+    default :
+      BG = 32;
+      vertical = 179;
+      horizontal = 196;
+      TL = 218;
+      TR = 191;
+      BL = 192;
+      BR = 217;
+  }
+  /*
+    RENDERING:
+    Rendering happens top to bottom, line for line.
+    A whole line is rendered  left-to-right, then
+    gotoxy() is called to move the cursor to the 
+    beginning of the next line. 
+    The title is then rendered on top, if necessary.
+  */
+
+  //Top line
+  gotoxy(ax, ay);
+  printf("%c",TL);
+  for(int i = ax+1; i < bx; i++){
+    printf("%c",HL);
+  }
+  printf("%c",TR);
+
+  //Middle lines
+  for(int i = ay+1; i < by; i++){
+    gotoxy(ax,i);
+    printf("%c",VL);
+    for(int j = ax+1; j < bx; j++){
+      printf("%c",BG);
+    }
+    printf("%c",VL);
+  }
+
+  //Bottom line
+  gotoxy(ax, by);
+  printf("%c",BL);
+  for(int i = ax+1; i < bx; i++){
+    printf("%c",HL);
+  }
+  printf("%c",BR);
+
+  //Title rendering
+  switch(style) {
+    case 1 :
+      gotoxy(ax+1, ay);
+      printf("%c",185);
+      printf("%s", title);
+      printf("%c",204);
+
+    default :
+    gotoxy(ax+1, ay+1);
+    printf("%s",title);
+  }
+
+}
+
+
---
+++ lookup.h
@@ -0,0 +1,17 @@
+// =====================================================================
+//
+//	Exported by Cearn's excellut v1.0
+//	(comments, kudos, flames to daytshen@hotmail.com)
+//
+// =====================================================================
+
+#ifndef LOOKUP_H
+#define LOOKUP_H
+
+// === LUT SIZES ===
+#define SIN_SIZE 512
+
+// === LUT DECLARATIONS ===
+extern const signed short SIN[512];
+
+#endif	// LOOKUP_H
