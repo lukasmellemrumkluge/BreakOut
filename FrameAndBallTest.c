90aada36d750f19d69ed435de109cb3f4a29d55b
lukasmellemrumkluge <39825381+lukasmellemrumkluge@users.noreply.github.com>
2018-06-06 11:13:41 +0200

    Headerfiles, renderAll and shorter runtime

--- FrameAndBallTest.c
+++ FrameAndBallTest.c
@@ -1,38 +1,34 @@
 
 #include "stm32f30x_conf.h" // STM32 config
 #include "30010_io.h" // Input/output library for this course
-#include <stdio.h>
-#define ESC 0x1B
-
-typedef struct {
-    int TLx;
-    int TLy;
-    int BRx;
-    int BRy;
-} frame_t;
-
-
+#include "PhysicsEngine.h" // Internal physics utilities
+#include "ansi.h" // Terminal Graphixxx
+#include <stdio.h>  //I/O functions of C
+#define ESC 0x1B //define escape
 
 int main(void) {
-    //initialize and draw border
+    //initialize border
     frame_t border;
     border.TLx = 1;
     border.TLy = 1;
     border.BRx = 10;
     border.BRy = 10;
-    window(border.TLx,border.TLy,border.BRx,border.BRy,0,0);
+    
     //initialize ball
     ball_t ball;
     ball.xpos = 6;
     ball.ypos = 7;
     ball.xv = 1;
     ball.yv = 1;
+    
+    //render first frame
+    renderAll(ball * ball_p, frame_p * frame_t);
+    
     //begin gameplay
-
     int i;
-    for(i = 0; i < 1000; i++) {
+    for(i = 0; i < 100; i++) {
         updatePostion(ball, border);
-        drawBall(ball);
+        renderAll(ball * ball_p, frame_p * frame_t);
     }
     while(1){}
 }
