f16e0693f30f1633c2f17361176fc0bd12141c45
krisdobal <39825321+krisdobal@users.noreply.github.com>
2018-06-06 16:06:44 +0200

    l.19 frame -> frame_p

--- inc/ansi.h
+++ inc/ansi.h
@@ -14,7 +14,7 @@ void underline(uint8_t on);
 void blink(uint8_t on);
 void inverse(uint8_t on);
 void reset();
-void window(frame_t * frame_p, int style, char * title);
+void window(frame_t * frame_p, int style, char * title_p);
 void renderBall(ball_t * ball_p);
 void renderAll(ball_t * ball_p, frame_t * frame_p);
 
