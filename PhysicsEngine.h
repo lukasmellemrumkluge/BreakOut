a2258baad419822a665e39330affd2a6c1f3f72c
lukasmellemrumkluge <39825381+lukasmellemrumkluge@users.noreply.github.com>
2018-06-06 10:47:22 +0200

    Create PhysicsEngine.h

---
+++ PhysicsEngine.h
@@ -0,0 +1,23 @@
+/* PhysicsEngine.h
+ * contains ball and frame struct, and prototypes for the physics functions.
+ */
+ 
+ //Ball struct containing position and velocity
+ typedef struct {
+    int xpos;
+    int ypos;
+    int xv;
+    int yv;
+} ball_t;
+
+//Frame struct containing coordinates of upper left and bottom right corner.
+typedef struct {
+    int TLx;
+    int TLy;
+    int BRx;
+    int BRy;
+} frame_t;
+
+//function declarations
+void reflect(int * pos_p, int wall, int * v_p);
+void updateBall(ball_t * ball_p, frame_t * frame_p);
