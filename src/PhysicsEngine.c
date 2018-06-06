ed8652a1d03b48cda2be2b3f842126aead9cb556
lukasmellemrumkluge <39825381+lukasmellemrumkluge@users.noreply.github.com>
2018-06-06 15:07:29 +0200

    Add files via upload

--- src/PhysicsEngine.c
+++ src/PhysicsEngine.c
@@ -1,21 +1,25 @@
 //.c file for all internal 'physics' functions and structs in the program
+#include "PhysicsEngine.h"
+
 
 //ball struct containing postion and velocity
-typedef struct {
-    int xpos;
-    int ypos;
-    int xv;
-    int yv;
-} ball_t;
+//typedef struct {
+//    int xpos;
+//    int ypos;
+//    int xv;
+//    int yv;
+//} ball_t;
+
+
 
 // general one-dimensional reflect function taking moving object position and
 // velocity plus statical object position.
 void reflect(int * pos_p, int wall, int * v_p){
     //This function assumes good input.
     //All checks must be made before calling it.
-    
+
     // TODO : Double check the math
-    
+
     int out = *v_p + (*pos_p - wall);
     if(*v_p > 0){
         *pos_p = wall - out;
@@ -24,7 +28,7 @@ void reflect(int * pos_p, int wall, int * v_p){
     }
 
     //Update vector
-    *v_p *= -1; 
+    *v_p *= -1;
     //TODO : Just flip the relevant bit (fixed point!?!)
 
 }
@@ -36,18 +40,18 @@ void updateBall(ball_t * ball_p, frame_t * frame_p) {
 
     // X
     if(ball_p->xpos + ball_p->xv < frame_p->TLx) {
-        reflect(&ball_p->xpos, ball_p->TLx, &ball_p->xv);
+        reflect(&ball_p->xpos, frame_p->TLx, &ball_p->xv);
     } else if (ball_p->xpos + ball_p->xv > frame_p->BRx) {
-        reflect(&ball_p->xpos, ball_p->BRx, &ball_p->xv);
+        reflect(&ball_p->xpos, frame_p->BRx, &ball_p->xv);
     } else{
         ball_p->xpos += ball_p->xv;
     }
 
     // Y
     if(ball_p->ypos + ball_p->yv < frame_p -> TLy) {
-        reflect(&ball_p->ypos, ball_p->TLy, &ball_p->yv);
+        reflect(&ball_p->ypos, frame_p->TLy, &ball_p->yv);
     } else if (ball_p->ypos + ball_p->yv > frame_p -> BRy) {
-        reflect(&ball_p->ypos, ball_p->BRy, &ball_p->yv);
+        reflect(&ball_p->ypos, frame_p->BRy, &ball_p->yv);
     } else{
         ball_p->ypos += ball_p->yv;
     }
