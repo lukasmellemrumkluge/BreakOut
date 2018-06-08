cbddef4c0e7bf5d21ecd3d148f74489e3c69e8b5
krisdobal <39825321+krisdobal@users.noreply.github.com>
2018-06-08 10:49:33 +0200

    Changed everything..

--- LCDtest.c
+++ LCDtest.c
@@ -1,25 +1,24 @@
+/*
+**
+**                           Main.c
+**
+**
+**********************************************************************/
+/*
+   Last committed:     $Revision: 00 $
+   Last changed by:    $Author: $
+   Last changed date:  $Date:  $
+   ID:                 $Id:  $
 
+**********************************************************************/
+#include "stm32f30x_conf.h"
 #include "30010_io.h"
 #include "charset.h"
+#include <string.h>
+//#include <stdio.h>
 
-void main() {
 
-	//Important LCD functions:
-
-	//Initialize the LCD
-	init_spi_lcd();
-
-	//initialize buffer of 512 8-bit vertical lines
-	uint8_t buffer[512];
-
-	//Set them all to be stripy (0xAA = 10101010)
-	memset(buffer,0xAA,512);
-
-	//Updates the LCD
-	lcd_push_buffer(buffer);
-}
-
-void lcd_update(char * text, int * readIndex, uint8_t line){
+void lcd_update(char * text, int * readIndex, uint8_t line, uint8_t * buffer){
 	//Scrolls
 
 	//TODO : only scroll sometimes
@@ -27,25 +26,27 @@ void lcd_update(char * text, int * readIndex, uint8_t line){
 	uint8_t offset = 128*line;
 
 	//Shift entire register
-	for(int i=offset; i<126+offset; i++){
-		buffer[i] = buffer[i+1]
+	for(int i=offset; i<127+offset; i++){
+		buffer[i] = buffer[i+1];
 	}
 
 	//Find next byte
 	char c = text[*readIndex/5] - 0x20;
 	buffer[127 + offset] = character_data[c][*readIndex%5];
-	*readIndex++;
+	(*readIndex)++;
+
+    lcd_push_buffer(buffer);
 
 	//Start from beginning of string when end is reached.
 	//Sizeof(text) works because a char only takes up one byte. -1 because of the end character
 	//TODO : Confirm this works as intended
-	if(readIndex/5 >= sizeof(text)-1){
-		readIndex = 0;
+	if((*readIndex)/5 >= strlen(text)){
+		(*readIndex) = 0;
 	}
 
 }
 
-void lcd_write_string(int slice, uint8_t line, char * text){
+void lcd_write_string(int slice, uint8_t line, char * text, uint8_t * buffer){
 	//Slices are 0-127 (relative coordinates)
 	//lines 0-3
 	//Negative and large slice vals supported
@@ -57,12 +58,12 @@ void lcd_write_string(int slice, uint8_t line, char * text){
 	// What about slice values over 127?
 
 	//Calculate number of lines in text.
-	int textLength = sizeof(text) / sizeof(text[0]);
+	int textLength = strlen(text);
 	int textLines = textLength*5;
 
 	//From relative slice to absolute slice
 	//slice += line*128;
-	
+
 	//Current char
 	uint8_t c;
 
@@ -72,6 +73,7 @@ void lcd_write_string(int slice, uint8_t line, char * text){
 	//Drawing position, in slices
 	int rendPos = slice;
 	int offset = line * 128;
+	//uint8_t breakflag = 0;
 	//Filling the required line of the buffer, once.
 	while(1){
 	//While loop supports multiple renderings of short words.
@@ -82,15 +84,15 @@ void lcd_write_string(int slice, uint8_t line, char * text){
 			//Set c to the current char corrected for the charset
 			c = text[i]-0x20;
 
-			if(rendPos < 0) {
+			if(rendPos < 0){
 				//Negative slice value:
 				// Neg. values are handled by stepping through them, while
 				// incrementing rendPos until the first char to be
-				// rendered is reached. Then it renders the first, 
+				// rendered is reached. Then it renders the first,
 				// partial char. The check for <5 is to catch half-chars.
 				if(rendPos < -4){
 					rendPos += 5;
-				} else {
+				} else{
 					for(int j = 0; j<5; j++){
 						if(rendPos >= 0){
 							buffer[rendPos+offset] = character_data[c][j];
@@ -109,17 +111,42 @@ void lcd_write_string(int slice, uint8_t line, char * text){
 			} else {
 				//Renders "tail", and catches end-of-render
 				readIndex = i*5;
-				for(uint8_t = 0; rendPos<128; i++){
-					buffer[offset+(rendPos++)] = character_data[c][i];
+				for(uint8_t j = 0; rendPos<128; j++){
+					buffer[offset+(rendPos++)] = character_data[c][j];
 					readIndex++;
 				}
-				break;
+				goto endlabel;
 			}
 		}
 	}
 
-	while(1){
-		lcd_update(text, &readIndex, line);
+
+	endlabel: while(1){
+	    for(int i = 0; i<200000;i++){}
+		lcd_update(text, &readIndex, line, buffer);
 	}
+
 }
+
+int main(){
+
+	//Important LCD functions:
+    init_usb_uart(9600);
+	//Initialize the LCD
+	init_spi_lcd();
+
+	//initialize buffer of 512 8-bit vertical lines
+	uint8_t buffer[512];
+
+	//Set them all to be stripy (0xAA = 10101010)
+	memset(buffer,0xAA,512);
+
+    char * text = "Come inside! Best pizza in towm! One slice - only 5.45! Only at Pierre´s! ";
+    lcd_write_string(10, 0, text, buffer);
+	//Updates the LCD
+	lcd_push_buffer(buffer);
+	while(1){}
+}
+
+
 
